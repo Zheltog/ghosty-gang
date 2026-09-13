2af83f0ab57da03efeeac5e03c0adbe3f9969ad1
Daniil Beloglazov <33455333+Zheltog@users.noreply.github.com>
2026-09-13 22:03:34 +0700

    Audio settings

---
+++ Scripts/Audio/Command/AudioSoundRandomPitchCommand.gd
@@ -0,0 +1,6 @@
+class_name AudioSoundRandomPitchCommand
+
+var resource_name: String
+var pitch_from: float
+var pitch_to: float
+var relative_volume: float = 100
--- Scripts/Audio/CommonAudioProcessor.gd
+++ Scripts/Audio/CommonAudioProcessor.gd
@@ -1,6 +1,5 @@
 extends Node
 
-const _volume_change_seconds: float = 1
 const _sound_players_pool_size: int = 10
 
 @onready var _music_player: TypedAudioStreamPlayer = $MusicPlayer
@@ -8,6 +7,7 @@ const _sound_players_pool_size: int = 10
 
 static var music_volume: float = 100
 static var sound_volume: float = 100
+static var relative_volumes: Dictionary = {}
 
 var _sound_players_pool: Array[TypedAudioStreamPlayer] = []
 var _looped_sound_players: Dictionary = {}
@@ -26,21 +26,27 @@ func process_volume_changed() -> void:
 	for i in range(_sound_players_pool_size):
 		_sound_players_pool[i].adjust_volume_instant()
 
-func process_music(instant: bool, post_action: String, resource_name: String) -> void:
-	_process_context(_music_player, instant, post_action, resource_name)
+func process_music(command: AudioMusicCommand) -> void:
+	_process_context(_music_player, command.instant, command.post_action, command.resource_name, \
+		command.adjustment_mode, command.adjustment_value, command.relative_volume)
 
-func process_sound(instant: bool, resource_name: String) -> void:
+func process_sound(command: AudioSoundCommand) -> void:
 	var player = _pick_sound_player()
 	_reset_player_pitch(player)
-	_process_context(player, instant, "", resource_name)
+	_process_context(player, command.instant, "", command.resource_name, \
+		command.adjustment_mode, command.adjustment_value, command.relative_volume)
 
-func process_sound_random_pitch(resource_name: String, pitch_from: float, pitch_to: float) -> void:
-	var random_pitch = randf_range(pitch_from, pitch_to)
+func process_sound_random_pitch(command: AudioSoundRandomPitchCommand) -> void:
+	var random_pitch = randf_range(command.pitch_from, command.pitch_to)
 	var player = _pick_sound_player()
 	player.pitch_scale = random_pitch
-	_process_context(player, true, "", resource_name)
+	# todo: no need to pass adjustment parameters at all
+	_process_context(player, true, "", command.resource_name, \
+		TypedAudioStreamPlayer.AdjustmentMode.BY_TIME, 0, command.relative_volume)
 
-func process_sound_looped(instant: bool, post_action: String, resource_name: String) -> void:
+func process_sound_looped(command: AudioSoundLoopedCoomand) -> void:
+	var resource_name = command.resource_name
+	var post_action = command.post_action
 	if resource_name == null or resource_name == "":
 		printerr("[CommonAudioProcessor] No looped sound resource name provided")
 		return
@@ -48,13 +54,15 @@ func process_sound_looped(instant: bool, post_action: String, resource_name: Str
 		if post_action == null or post_action == "":
 			printerr("[CommonAudioProcessor] Post action should be provided for batch processing")
 			return
-		_process_post_action_for_all(post_action, instant)
+		_process_post_action_for_all(post_action, command.instant, \
+			command.adjustment_mode, command.adjustment_value)
 		return
 	if post_action != null and post_action != "":
 		if not _looped_sound_players.has(resource_name):
 			printerr("[CommonAudioProcessor] Looped sound is not processing: ", resource_name)
 			return
-		_process_post_action(_looped_sound_players[resource_name], post_action, instant)
+		_process_post_action(_looped_sound_players[resource_name], post_action, command.instant, \
+			command.adjustment_mode, command.adjustment_value)
 		if post_action == AudioConstants.STOP:
 			_looped_sound_players.erase(resource_name)
 		return
@@ -64,51 +72,57 @@ func process_sound_looped(instant: bool, post_action: String, resource_name: Str
 	var sound_player = _pick_sound_player()
 	_reset_player_pitch(sound_player)
 	_looped_sound_players[resource_name] = sound_player
-	_process_resource(sound_player, resource_name, instant)
+	_process_resource(sound_player, resource_name, command.instant, command.adjustment_mode, \
+		command.adjustment_value, command.relative_volume)
 
-func _process_context(player: TypedAudioStreamPlayer, instant: bool, post_action: String, resource_name: String) -> void:
+func _process_context(player: TypedAudioStreamPlayer, instant: bool, post_action: String, resource_name: String, \
+	adjustment_mode: TypedAudioStreamPlayer.AdjustmentMode, adjustment_value: float, relative_volume: float) -> void:
 	if post_action != null and post_action != "":
-		_process_post_action(player, post_action, instant)
+		_process_post_action(player, post_action, instant, adjustment_mode, adjustment_value)
 		return
 	if resource_name == null or resource_name == "":
 		printerr("[CommonAudioProcessor] No resource name")
 		return
-	_process_resource(player, resource_name, instant)
+	_process_resource(player, resource_name, instant, adjustment_mode, adjustment_value, relative_volume)
 
-func _process_post_action_for_all(post_action: String, instant: bool) -> void:
+func _process_post_action_for_all(post_action: String, instant: bool, \
+	adjustment_mode: TypedAudioStreamPlayer.AdjustmentMode, adjustment_value: float) -> void:
 	for player in _looped_sound_players.values():
-		_process_post_action(player, post_action, instant)
+		_process_post_action(player, post_action, instant, adjustment_mode, adjustment_value)
 	if post_action == AudioConstants.STOP:
 		_looped_sound_players.clear()
 
-func _process_post_action(player: TypedAudioStreamPlayer, post_action: String, instant: bool) -> void:
+func _process_post_action(player: TypedAudioStreamPlayer, post_action: String, instant: bool, \
+	adjustment_mode: TypedAudioStreamPlayer.AdjustmentMode, adjustment_value: float) -> void:
 	match post_action:
 		AudioConstants.STOP:
 			if instant:
 				player.stop()
 			else:
-				player.adjust_volume_falling(_volume_change_seconds, func(): player.stop())
+				player.adjust_volume_falling(adjustment_mode, adjustment_value, func(): player.stop())
 		AudioConstants.PAUSE:
 			if instant:
 				player.stream_paused = true
 			else:
-				player.adjust_volume_falling(_volume_change_seconds, func(): player.stream_paused = true)
+				player.adjust_volume_falling(adjustment_mode, adjustment_value, func(): player.stream_paused = true)
 		AudioConstants.RESUME:
 			if instant:
 				player.adjust_volume_instant()
 			else:
-				player.adjust_volume_rising(_volume_change_seconds)
+				player.adjust_volume_rising(adjustment_mode, adjustment_value)
 			player.stream_paused = false
 		_:
 			printerr("[CommonAudioProcessor] Unknown post action: ", post_action)
 
-func _process_resource(player: TypedAudioStreamPlayer, resource_name: String, instant: bool) -> void:
+func _process_resource(player: TypedAudioStreamPlayer, resource_name: String, instant: bool, \
+	adjustment_mode: TypedAudioStreamPlayer.AdjustmentMode, adjustment_value: float, relative_volume: float) -> void:
+	player.play_resource(resource_name, _get_cached_stream(resource_name))
+	if relative_volume != 100:
+		relative_volumes[resource_name] = relative_volume
 	if instant:
 		player.adjust_volume_instant()
 	else:
-		player.adjust_volume_rising(_volume_change_seconds)
-	player.stream = _get_cached_stream(resource_name)
-	player.play()
+		player.adjust_volume_rising(adjustment_mode, adjustment_value)
 
 func _pick_sound_player() -> TypedAudioStreamPlayer:
 	if _sound_players_pool_size == 0:
